7e78b95e1c5a3c77d6797a539d36635ecefbccaf
Franco Zurita Perea <francozurita@gmail.com>
2024-02-15 09:02:49 -0300

    agregar cuil o zoneinfo

--- src/django_keycloak/services/oidc_profile.py
+++ src/django_keycloak/services/oidc_profile.py
@@ -103,14 +103,30 @@ def update_or_create_user_and_oidc_profile(client, id_token_object):
     with transaction.atomic():
         UserModel = get_user_model()
         email_field_name = UserModel.get_email_field_name()
-        user, _ = UserModel.objects.update_or_create(
-            username=id_token_object['preferred_username'],  # modified to map with the username
-            defaults={
-                email_field_name: id_token_object.get('email', ''),
-                'first_name': id_token_object.get('given_name', ''),
-                'last_name': id_token_object.get('family_name', '')
-            }
-        )
+
+        if hasattr(UserModel, 'cuil'):
+            cuil_or_zoneinfo = id_token_object.get('cuit', '') if 'cuit' in id_token_object else id_token_object.get(
+                'zoneinfo', '')
+            # El modelo de usuario tiene el atributo 'cuil', podemos usarlo
+            user, _ = UserModel.objects.update_or_create(
+                username=id_token_object['preferred_username'],
+                defaults={
+                    email_field_name: id_token_object.get('email', ''),
+                    'first_name': id_token_object.get('given_name', ''),
+                    'last_name': id_token_object.get('family_name', ''),
+                    'cuil': cuil_or_zoneinfo,
+                }
+            )
+        else:
+            # El modelo de usuario no tiene el atributo 'cuil'
+            user, _ = UserModel.objects.update_or_create(
+                username=id_token_object['preferred_username'],
+                defaults={
+                    email_field_name: id_token_object.get('email', ''),
+                    'first_name': id_token_object.get('given_name', ''),
+                    'last_name': id_token_object.get('family_name', ''),
+                }
+            )
 
         oidc_profile, _ = OpenIdConnectProfileModel.objects.update_or_create(
             sub=id_token_object['sub'],
